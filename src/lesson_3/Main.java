2fb78eb91ff982b54982eef5e0906f0c9c826340
MT1000 <MT.1000@yandex.ru>
2021-12-05 13:35:57 +0500

    Работает, проблема только с крайними значениями

---
+++ src/lesson_3/Main.java
@@ -0,0 +1,52 @@
+package lesson_3;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16};
+
+        int missElement = findMissElementBinary(array);
+
+        System.out.println("Потерянный элемент = " + missElement);
+    }
+
+    private static int findMissElementBinary(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        System.out.println("Длина массива " + end);
+        int base;
+        //int i = 0;
+        int missElement = -1;
+        //int middleArray = array.length/2;
+
+        while (missElement == -1){
+
+            base = (start + end) / 2;
+            System.out.println("Base " + base);
+
+            if (array[base - 1] == base + 1 && array[base - 2] == base - 1) {
+                missElement = base;
+                System.out.println("missElement " + missElement);
+            } else if (array[base + 1] - array[base] != 1) {
+                missElement = base + 2;
+                System.out.println("missElement " + missElement);
+            }
+
+            else if (array[base - 1] == base) {
+                start = base + 1;
+                System.out.println("Start " + start);
+            }
+            else if (array[base - 1] > base) {
+                end = base - 1;
+                System.out.println("End " + end);
+            }
+
+        }
+
+        return missElement;
+    }
+
+}
